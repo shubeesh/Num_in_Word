5b91e3f711c23ad51af8443dc4d3c02c716dedea
shubeeth001 <shubeeth001@warren.k12.in.us>
2024-09-10 12:55:43 -0400

    numinword

--- src/Main.java
+++ src/Main.java
@@ -20,24 +20,34 @@ public class Main {
         switch (number) {
             case 0:
                 System.out.println("ZERO");
+                break;
             case 1:
                 System.out.println("ONE");
+                break;
             case 2:
                 System.out.println("TWO");
+                break;
             case 3:
                 System.out.println("THREE");
+                break;
             case 4:
                 System.out.println("FOUR");
+                break;
             case 5:
                 System.out.println("FIVE");
+                break;
             case 6:
                 System.out.println("SIX");
+                break;
             case 7:
                 System.out.println("SEVEN");
+                break;
             case 8:
                 System.out.println("EIGHT");
+                break;
             case 9:
                 System.out.println("NINE");
+                break;
             default:
                 System.out.println("OTHER");
                 break;
@@ -47,26 +57,37 @@ public class Main {
         switch (number) {
             case 0:
                 System.out.println("ZERO");
+                break;
             case 1:
                 System.out.println("ONE");
+                break;
             case 2:
                 System.out.println("TWO");
+                break;
             case 3:
                 System.out.println("THREE");
+                break;
             case 4:
                 System.out.println("FOUR");
+                break;
             case 5:
                 System.out.println("FIVE");
+                break;
             case 6:
                 System.out.println("SIX");
+                break;
             case 7:
                 System.out.println("SEVEN");
+                break;
             case 8:
                 System.out.println("EIGHT");
+                break;
             case 9:
                 System.out.println("NINE");
+                break;
             default:
                 System.out.println("OTHER");
+                break;
         }
     }
 }
